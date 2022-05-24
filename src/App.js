d833c39b5d24ac21f90f9917c0458dcdff486cad
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2022-05-24 19:19:41 +0200

    change middle page message

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was changed</p>
+        <p>Changed!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the Feature branch
+          Learn React
         </a>
       </header>
     </div>
